cc172158b9a06ba9542a0a11f22a3b63420a6e93
Gummientchen <steff.freak@gmail.com>
2019-11-13 14:42:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -68,6 +68,7 @@ if [ "$1" = "import" ]; then
     gdal_contour -i 10 -a height srtm_30m.tif srtm_30m_contours_10m
     cd srtm_30m_contours_10m
     shp2pgsql -p -I -g way -s 4326:900913 contour.shp contour | sudo -u postgres psql -d gis
+    shp2pgsql -a -g way -s 4326:900913 contour.shp contour | sudo -u postgres psql -d gis
     cd ..
 
     # Register that data has changed for mod_tile caching purposes
